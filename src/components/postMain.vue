c6fe799d9a5cbd4d401b4c78240d662482948384
jackmrzhou <594780735@qq.com>
2020-01-09 06:55:49 -0800

    sync

--- src/components/postMain.vue
+++ src/components/postMain.vue
@@ -1,6 +1,32 @@
 <template>
     <el-card shadow="always">
         <el-tabs v-model="activeName">
+            <el-tab-pane label="update" name="update">
+                <el-table
+                    :data="postsList"
+                    style="width: 100%"
+                    height="70vh">
+                    <el-table-column
+                        prop="date"
+                        label="Date"
+                        width="180">
+                    </el-table-column>
+                    <el-table-column
+                        prop="title"
+                        label="Title"
+                        width="180">
+                    </el-table-column>
+                    <el-table-column
+                        prop="beginning"
+                        label="Beginning">
+                    </el-table-column>
+                    <el-table-column label="operation" width="100">
+                        <template slot-scope="scope">
+                            <el-button @click="update(scope)" type="text" size="medium">Edit</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </el-tab-pane>
             <el-tab-pane label="write" name="write">
                 <el-form ref="CreatePost" :model="PostForm" label-position="top" label-width="80px">
                     <el-form-item label="Title" prop="title">
@@ -44,6 +70,75 @@ export default {
                 date: '',
                 content: ''
             },
+            postsList: [{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-11",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            },{
+                date: "2019-12-19",
+                title: "what a wonderful world",
+                beginning: "what a wonderful world..."
+            }],
             postUrl: '/api/post'
         }
     }, 
@@ -56,6 +151,9 @@ export default {
         }
     },
     methods: {
+        update(row) {
+            console.log(row)
+        },
         onSubmit() {
             //console.log(this.PostForm)
             var raw_content = "---\n"
